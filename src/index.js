fb2616a5187d272a7ac144062eb2e9141a0d4adc
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-03-01 14:44:25 +0200

    markup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+import './styles.css';
+import menu from './menu.json';
+import template from './template.hbs';
+const markup = menu.reduce((acc, dish) => {
+  return acc + template(dish);
+}, '');
+const container = document.querySelector('.js-menu');
+container.insertAdjacentHTML('afterbegin', markup);
